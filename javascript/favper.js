6b33e8df3f8b245d04ca10ce67ff99d5755e1283
hasan123712 <supastrikashakesrasta@gmail.com>
2026-07-19 14:51:35 -0400

    ok

---
+++ javascript/favper.js
@@ -0,0 +1,9 @@
+// the person
+
+let person = {
+    name: "Sameera Nazneen"
+    relationship: "Mother"
+    activity:"Shopping"
+    city: "Ottawa"
+
+}
---
+++ javascript/odsa.js
@@ -0,0 +1,6 @@
+let car = ["Bmw", "Mustang", "Supra", "Honda", "Bugatti"]
+
+
+console.log(car[0])
+console.log(car[2])
+console.log(car)
